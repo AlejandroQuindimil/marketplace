98ad4153039101866751cc3b5043bb05528c6742
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2026-07-15 12:51:24 +0200

    feat: rename demo to marketplace-backend

--- demo/src/test/java/com/example/demo/ApplicationTests.java
+++ demo/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MarketplaceApplicationTests {
 
 	@Test
 	void contextLoads() {
