1cf574748fee87d06e79878cce694b247d86e667
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2026-08-29 17:25:23 +0200

    fix: usar cluster de MongoDB Atlas en vez de Mongo local Spring Boot 4 cambio el prefijo de la propiedad de spring.data.mongodb a spring.mongodb, por eso la app seguia conectando a localhost pese a tener bien la URI de Atlas. Corregido cambiando la clave.

--- demo/src/main/java/com/drip/marketplace/MarketplaceApplication.java
+++ demo/src/main/java/com/drip/marketplace/MarketplaceApplication.java
@@ -10,8 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class MarketplaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketplaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceApplication.class, args);
+    }
 
 }
