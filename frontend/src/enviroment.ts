5f68109bc5d423bd7e376181352424e8cfd80112
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2026-08-29 18:34:22 +0200

    feat: cambiar  la conexion backend

--- frontend/src/enviroment.ts
+++ frontend/src/enviroment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  //apiUrl: 'http://localhost:8080/api'//local 
+
+  apiUrl: 'https://marketplace-wa7t.onrender.com' //render
 };
