14818971b0505753e501098bc7ac74f71c07ad52
AlejandroQuindimil <alejandro.quindimil.cancio@gmail.com>
2026-08-29 18:40:00 +0200

    feat camniar el backend de local a render

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  //Local
+  //apiUrl: 'http://localhost:8080/api'
+
+  //Render
+  apiUrl: 'https://marketplace-wa7t.onrender.com'
 };
